722895dcaf5bd74f68bc0b1fcb7bf16aa5f49d82
Haixiang <haixiang6123@gmail.com>
2019-04-01 12:56:34 -0700

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {MessagesComponent} from './messages/messages.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +21,10 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
